645c0ca7deccd8fdf3f334ef73a9295422d87fe0
TiilS <ilyaagildin@yandex.ru>
2024-05-02 23:59:40 +0300

    Format

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
-    std::string name; // name.
+    string name; // name.
     std::cin >> name; // input.
     std::cout << "Hello world from " << name << std::endl; // output
     return 0;
