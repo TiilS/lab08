250fc6c6ee8e22e167d43f4ac46da5e4c60d4ae1
TiilS <ilyaagildin@yandex.ru>
2024-05-02 23:27:34 +0300

    Remake HW

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 int main(){
-    cout << "Hello world!" << endl;
+    std::string name; // name
+    std::cin >> name; // input
+    std::cout << "Hello world from " << name << std::endl; // output
     return 0;
 }
