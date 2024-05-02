71eccfed0ed07b7d8d3e81d83cd6d7c03fbc1a4d
TiilS <75080702+TiilS@users.noreply.github.com>
2024-05-02 23:47:04 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main(){
-    std::string name; // name
-    std::cin >> name; // input
+    std::string name; // name.
+    std::cin >> name; // input.
     std::cout << "Hello world from " << name << std::endl; // output
     return 0;
 }
